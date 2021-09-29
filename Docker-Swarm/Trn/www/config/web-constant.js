45a7a78e1bdec350159ebb218e8d8c65c69caf6e
phattanothai_p <phattanothai_p@empatech.tech>
2021-09-29 20:12:44 +0700

    update

---
+++ Docker-Swarm/Trn/www/config/web-constant.js
@@ -0,0 +1,6 @@
+const API = {
+   auth: `http://localhost:8000/api/auth`,
+   staff: `http://localhost:8000/api/staff`,
+   member: `http://localhost:8000/api/member`,
+   masterData: `http://localhost:8000/api/master-data`
+};
